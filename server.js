837f96e8ad29bf20e521779513b59fc0f519b8dc
Plinio Santana <pliniosb@gmail.com>
2019-10-31 12:25:41 -0300

    Regex update

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.get('/api/timestamp/:time?', (req, res) => {
   try {
     const { time } = req.params;
 
-    let check_time
+    let check_time;
 
     if(time)
-     check_time = time.search(/(\d{4}-\d{2}-\d{2}|[\d-]+)/g);
+     check_time = time.search(/((\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))|^\d+$)/);
 
     if(check_time === -1)
       throw("Invalid Date");
@@ -21,7 +21,7 @@ app.get('/api/timestamp/:time?', (req, res) => {
     let time_unix = (time) ? new Date((time.search(/-/g) > -1) ? time : parseInt(time)).getTime() : new Date().getTime();
     let time_utc = (time) ? new Date((time.search(/-/g) > -1) ? time : parseInt(time)).toUTCString() : new Date().toUTCString();
 
-    res.json({unix: time_unix, utc: time_utc})
+    res.json({unix: time_unix, utc: time_utc});
   } 
   catch (error) {
     res.status(500).json({"error": error});  
